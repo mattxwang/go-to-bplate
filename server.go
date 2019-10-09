45b57171fc256de506ad87bf67c1121e5519b575
Matthew Wang <matt@matthewwang.me>
2019-10-09 16:35:58 -0700

    implements in-memory cache
    it's alllll coming together

--- server.go
+++ server.go
@@ -10,21 +10,45 @@ import (
 const jsonContentType = "application/json"
 
 type ResponseServer struct {
+	cache map[string]*MenuHit
 	http.Handler
+}
+
+type MenuHit struct {
+	//time Time 
+	data *DayData
 }
 
 func NewResponseServer() *ResponseServer {
 	s := new(ResponseServer)
+	s.cache = map[string]*MenuHit{}
 	router := http.NewServeMux()
 	router.Handle("/today", http.HandlerFunc(s.todayHandler))
 	s.Handler = router
 	return s
 }
 
+func NewMenuHit(data *DayData) *MenuHit{
+	m := new(MenuHit)
+	m.data = data 
+	return m
+}
+
 func (p *ResponseServer) todayHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", jsonContentType)
 	dateString := time.Now().Format("2006-01-02")
 	fmt.Println("Getting all meals for " + dateString)
-	dayData := fetchDayData(dateString, []string{}, []string{}, []string{})
+	hit, exists := p.cache[dateString]
+	if !exists {
+		fmt.Println("Cache miss, retrieving from server")
+		newDayData := fetchDayData(dateString, []string{}, []string{}, []string{})
+		fmt.Println("Server responded, caching result for future")
+		newMenuHit := NewMenuHit(newDayData)
+		p.cache[dateString] = newMenuHit
+		hit = newMenuHit
+	} else {
+		fmt.Println("Cache hit!")
+	}
+	dayData := hit.data
     json.NewEncoder(w).Encode(dayData)
 }
